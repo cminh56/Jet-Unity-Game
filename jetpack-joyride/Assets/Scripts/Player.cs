9538790b4f737a13298b0bc13a4259e2d23d2b2a
chinhminh <chinhminh2003@gmail.com>
2024-11-20 11:55:19 +0700

    Jet unity game

---
+++ jetpack-joyride/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+	
+	Rigidbody body;
+	
+	public bool gameOver = false;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+		if (gameOver) {
+			
+			if (Input.GetKey(KeyCode.Space)) {
+				SceneManager.LoadScene("Game");
+			}
+			return;
+		}
+		if (Input.GetKey(KeyCode.Space)) {
+			body.AddForce(new Vector3(0, 50,0), ForceMode.Acceleration);
+		} else if (Input.GetKey(KeyCode.Space)) {
+			body.velocity *= 0.15f;
+		}
+	}
+	
+	void OnTriggerEnter(Collider collider) {
+		gameOver = true;
+		body.isKinematic = true;
+	}
+}
